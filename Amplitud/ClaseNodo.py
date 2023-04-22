98b2d733830b18d467ddc28bc2e24202518aa71b
Nicol Ortiz <nicolvaleria0919@gmail.com>
2023-04-22 18:32:54 -0500

    primer commit

---
+++ Amplitud/ClaseNodo.py
@@ -0,0 +1,78 @@
+import copy
+
+# 1.  Leer archivo .txt
+with open("Prueba_Amplitud.txt", 'r') as f:
+    matriz_string = f.read()
+
+matriz = []
+#Convertir el string a una matriz
+for linea in matriz_string.split('\n'):
+    matriz.append(linea.split(' '))   
+
+#Ubicar posición del elemento
+def ubicarElemento (matriz, elementoabuscar):
+    coordenadas = []
+    for fila in matriz:
+        for elemento in fila:
+            if elemento == elementoabuscar:
+                coordenadas.append(matriz.index(fila))
+                coordenadas.append(fila.index(elemento))
+                return coordenadas
+    return -1
+
+
+# Clase Nodo
+class Nodo:
+    #Constructor
+    def __init__(self, estado, padre,operador):
+        self.estado = estado
+        self.padre = padre
+        self.operador = operador
+        self.profundidad = 0
+
+    #Métodos getters
+    def get_padre(self):
+        return self.padre
+    
+    def get_estado(self):
+        return self.estado
+    
+    #Método que verifica si la fila y la columna del nodo existen en la matriz
+    def verificarExistencia (self, fila, columna):
+        return fila > -1 and fila < len(matriz[0]) and columna > -1 and columna < len(matriz)
+    
+    #Método para mover elemento
+    def moverElemento(self,matriz):
+        # Ubicar a gokú y sus coordenadas
+        elemento = ubicarElemento(matriz, '2')
+        fila = elemento[0]
+        columna = elemento[1]
+
+        # Variable necesaria
+        movimientos = []
+
+        # Intentar realizar cada movimiento válido
+        for row, column, operador in [(0, 1, "derecha"), (0, -1, "izquierda"), (-1, 0, "subir"), (1, 0, "bajar")]:
+            # Verificar si el movimiento es válido
+            if self.verificarExistencia(fila + row, columna + column) and matriz[fila + row][columna + column] != '1':
+                #copiar matriz original
+                matriz_aux = copy.deepcopy(matriz)
+                # Actualizar la matriz con el movimiento
+                matriz_aux[fila + row][columna + column] = '2'
+                matriz_aux[fila][columna] = '0'
+                # Agregar la actualización al arreglo    
+                movimientos.append((matriz_aux, operador))
+
+        # Devolver el arreglo de movimientos válidos
+        return movimientos
+
+    #Método que aumenta la profundidad del nodo
+    def modificarProfundidad (self):
+        if (self.padre != None):
+            self.profundidad = self.padre.profundidad +1    
+
+    # Método meta
+    def esMeta (self, matriz):
+        #Si la matriz no contiene esferas, es meta
+        return ubicarElemento(matriz,'6') == -1
+                 
---
+++ Amplitud/main.py
@@ -0,0 +1,63 @@
+from ClaseNodo import Nodo
+from  ClaseNodo import matriz
+import time
+
+# Aplicación del algoritmo
+#Recorrer la matriz por amplitud
+
+#Variables necesarias
+cola = [] #Guadará los nodos hijos
+expandidos = [] #Guadará los nodos expandidos
+
+#Función recorrer matriz
+def recorrerMatriz ():
+    #Definir padre inicial
+    padre = Nodo(matriz,None,None)
+    #Agregar padre a la cola
+    cola.append(padre)
+    #Variable que contendrá el primer elemeto de la cola, es decir el padre
+    padre_expandido = 0
+
+    #Ejecute mientras la cola este llena
+    while len(cola) != 0:
+        #Sacar padre de la cola
+        padre_expandido = cola.pop(0)
+
+        #Verificar si es meta
+        if padre_expandido.esMeta(padre_expandido.estado) == True:
+            #Añada el nodo meta a la lista expandidos y salgase del while
+            expandidos.append(padre_expandido)  
+            return
+            
+        #Crear hijos    
+        for nueva_matriz, operador in padre_expandido.moverElemento(padre_expandido.estado):    
+            #Cree el nodo hijo
+            hijo = Nodo(nueva_matriz, padre_expandido, operador)
+            #llamar el método prfundidad para que modifique el valor del atributo.
+            hijo.modificarProfundidad()
+
+            #Si es la raíz o si el estado del hijo y el estado del abuelo son diferentes, entonces 
+            if (padre_expandido.profundidad == 0 or hijo.get_estado() != padre_expandido.get_padre().get_estado()):
+                #Agreguelo al final de la cola
+                cola.append(hijo)
+
+        #Agregar padre a la lista de nodos espandidos
+        expandidos.append(padre_expandido)    
+    print('No se puedo encontrar todas las esferas')
+
+#Función calcular tiempo de implementación de la función recorrerMatriz
+def calcularDuracion ():
+    #Hora inicio
+    inicio = time.time()
+    recorrerMatriz()
+    #Hora finalización
+    fin = time.time() 
+    #Diferencia entre tiempos para ver el tiempo gastado
+    return fin - inicio
+
+#Guardar tiempo en la variable 
+tiempo_total = calcularDuracion()
+
+recorrerMatriz()
+
+
