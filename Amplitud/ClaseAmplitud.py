747b74dcf2f6d676a38c7cd6ff3fb1dccdcf3c96
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-04-23 14:09:25 -0500

    cambio de imagenes y de nombres

--- Amplitud/ClaseAmplitud.py
+++ Amplitud/ClaseAmplitud.py
@@ -1,63 +1,14 @@
 import copy
-import time
-import pygame
 
-# Variables
-matriz = []
-tamaño_ventana = 50
-
-# Leer archivo .txt
-with open('Prueba1.txt', 'r') as f:
+# 1.  Leer archivo .txt
+with open("Prueba_Amplitud.txt", 'r') as f:
     matriz_string = f.read()
 
-# Convertir el string a una matriz
+matriz = []
+#Convertir el string a una matriz
 for linea in matriz_string.split('\n'):
-    matriz.append([int(valor) for valor in linea.split(' ')])
-
-# Mostrar la matriz
-pygame.init()
-ventana = pygame.display.set_mode((len(matriz[0])*tamaño_ventana, len(matriz)*tamaño_ventana))
-pygame.display.set_caption('Matriz')
-
-img_suelo = pygame.image.load('.\imagenes\suelo.jpg')
-img_pared = pygame.image.load('.\imagenes\pared.jpg')
-img_goku = pygame.image.load('.\imagenes\goku.jpg')
-img_freezer = pygame.image.load('./imagenes/freezer.jpg')
-img_cell = pygame.image.load('.\imagenes\cell.jpg')
-img_semilla = pygame.image.load('.\imagenes\semilla.jpg')
-img_esfera = pygame.image.load('.\imagenes\esfera.jpg')
-
-
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        if matriz[i][j] == 0:
-            ventana.blit(img_suelo, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 1:
-            ventana.blit(img_pared, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 2:
-            ventana.blit(img_goku, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 3:
-            ventana.blit(img_freezer, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 4:
-            ventana.blit(img_cell, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 5:
-            ventana.blit(img_semilla, (j*tamaño_ventana, i*tamaño_ventana))
-        elif matriz[i][j] == 6:
-            ventana.blit(img_esfera, (j*tamaño_ventana, i*tamaño_ventana))
-
-# Actualizar la ventana
-pygame.display.update()
-
-# Esperar a que el usuario cierre la ventana
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-# 3.  Se aplicará búsqueda por amplitud
-
-# 4.  Aplicación del algoritmo
+    matriz.append(linea.split(' '))
+      
 
 #Ubicar posición del elemento
 def ubicarElemento (matriz, elementoabuscar):
@@ -70,10 +21,11 @@ def ubicarElemento (matriz, elementoabuscar):
                 return coordenadas
     return -1
 
+
 # Clase Nodo
 class Nodo:
     #Constructor
-    def __init__(self, estado, padre, operador):
+    def __init__(self, estado, padre,operador):
         self.estado = estado
         self.padre = padre
         self.operador = operador
@@ -85,9 +37,6 @@ class Nodo:
     
     def get_estado(self):
         return self.estado
-    
-    def get_operador(self):
-        return self.operador
     
     #Método que verifica si la fila y la columna del nodo existen en la matriz
     def verificarExistencia (self, fila, columna):
@@ -128,10 +77,3 @@ class Nodo:
         #Si la matriz no contiene esferas, es meta
         return ubicarElemento(matriz,'6') == -1
                  
-
-
-# Función profundidad del árbol
-
-#Recorrer la matriz por amplitud
-
-
--- Amplitud/main.py
+++ Amplitud/main.py
@@ -1,62 +1,114 @@
-from ClaseNodo import Nodo
-from  ClaseNodo import matriz
+import tkinter as tk
+from Recorrido import tiempo_total, expandidos, matriz
+from PIL import Image, ImageTk
 import time
 
-# Aplicación del algoritmo
-#Recorrer la matriz por amplitud
+#Guardar en una variable las matrices de los nodos en la lista expandidos
+nodo_actual = expandidos[len(expandidos)-1]
+caminos = []
+caminos_final = []
+while nodo_actual.padre is not None:
+    nodo_actual = nodo_actual.padre
+    caminos.append(nodo_actual.estado)
+caminos.insert(0,expandidos[len(expandidos)-1].estado) 
+caminos_final = caminos[::-1] #Arreglo que contiene los estados válidos desde la raíz hasta el final   
 
-#Variables necesarias
-cola = [] #Guadará los nodos hijos
-expandidos = [] #Guadará los nodos expandidos
+ventana = tk.Tk()
 
-#Función recorrer matriz
-def recorrerMatriz ():
-    #Definir padre inicial
-    padre = Nodo(matriz,None,None)
-    #Agregar padre a la cola
-    cola.append(padre)
-    #Variable que contendrá el primer elemeto de la cola, es decir el padre
-    padre_expandido = 0
+# Obtener el ancho y la altura de la pantalla
+ancho_pantalla = ventana.winfo_screenwidth()
+altura_pantalla = ventana.winfo_screenheight()
 
-    #Ejecute mientras la cola este llena
-    while len(cola) != 0:
-        #Sacar padre de la cola
-        padre_expandido = cola.pop(0)
+# Calcular la posición x e y de la ventana para centrarla
+x = int((ancho_pantalla - 1150) / 2)  # ancho de la ventana es 800
+y = int((altura_pantalla - 700) / 2)  # altura de la ventana es 600
 
-        #Verificar si es meta
-        if padre_expandido.esMeta(padre_expandido.estado) == True:
-            #Añada el nodo meta a la lista expandidos y salgase del while
-            expandidos.append(padre_expandido)  
-            return
-            
-        #Crear hijos    
-        for nueva_matriz, operador in padre_expandido.moverElemento(padre_expandido.estado):    
-            #Cree el nodo hijo
-            hijo = Nodo(nueva_matriz, padre_expandido, operador)
-            #llamar el método prfundidad para que modifique el valor del atributo.
-            hijo.modificarProfundidad()
+# Configurar la posición de la ventana
+ventana.geometry(f"1150x700+{x}+{y}")
 
-            #Si es la raíz o si el estado del hijo y el estado del abuelo son diferentes, entonces 
-            if (padre_expandido.profundidad == 0 or hijo.get_estado() != padre_expandido.get_padre().get_estado()):
-                #Agreguelo al final de la cola
-                cola.append(hijo)
+# Tamaño de cada celda
+cell_width = 50
+cell_height = 50
 
-        #Agregar padre a la lista de nodos espandidos
-        expandidos.append(padre_expandido)    
-    print('No se puedo encontrar todas las esferas')
+# Crear el canvas y dibujar el cuadrado 
+canvas = tk.Canvas(ventana, width=cell_width*10, height=cell_height*10) #Ancho del widget canvas
+canvas.place(x=60, y=60, width=cell_width*10, height=cell_height*10) #Agregar canvas a la ventana
+cuadrado_matriz = canvas.create_rectangle(0,0,cell_width*10,cell_height*10)
+canvas.itemconfig(cuadrado_matriz, outline="black", width=7)
 
-#Función calcular tiempo de implementación de la función recorrerMatriz
-def calcularDuracion ():
-    #Hora inicio
-    inicio = time.time()
-    recorrerMatriz()
-    #Hora finalización
-    fin = time.time() 
-    #Diferencia entre tiempos para ver el tiempo gastado
-    return fin - inicio
+# Dibujar la cuadrícula
+for i in range(10):
+    canvas.create_line(i*cell_width, 0, i*cell_width, cell_height*10) #Dibuja líneas verticales
+    canvas.create_line(0, i*cell_height, cell_width*10, i*cell_height) #Dibuja líneas horizontales
 
-#Guardar tiempo en la variable 
-tiempo_total = calcularDuracion()
+imagen = None
+canvas.imagenes = []
+
+# Dibujar el contenido de cada celda
+def dibujarImagen (url,etiqueta,x,y,width,height):
+    imagen = Image.open(url)
+    imagen = imagen.resize((x,y))
+    imagen = ImageTk.PhotoImage(imagen)
+    canvas.create_image(width, height, anchor=tk.NW, image=imagen, tag=etiqueta)
+    canvas.imagenes.append(imagen)
+
+# Variable global para guardar la referencia de la imagen
+imagenT = None
+
+def mostrarReporte():
+    global imagenT
+    
+    profun = expandidos[len(expandidos)-1].profundidad
+    profun_string = str(profun)
+    cuadro_info = tk.Canvas(ventana, width=400, height=400)
+    cuadro_info.place(x=580, y=50, width=600, height=600)
+    cuadro_info.create_rectangle(50, 100, 500, 400, fill='white')
+    cuadro_info.create_text(250, 160, text='La profundidad de la solución es: ' + profun_string, width=400, font=("Arial", 15))
+    cuadro_info.create_text(250, 190, text='Cantidad de nodos expandidos: ' + str(len(expandidos)) + ' nodos', width=400, font=("Arial", 15))
+    cuadro_info.create_text(250, 220, text='Tiempo de cómputo: ' + str(round(tiempo_total, 3)) + ' segundos', width=400, font=("Arial", 15))
+    imagen = Image.open("../imagenes/muñeca.png")
+    imagen = imagen.resize((150, 150))
+    imagenT = ImageTk.PhotoImage(imagen)
+    cuadro_info.create_image(270, 300, image=imagenT)
+
+#Función que muestra la matriz dada en la ventana
+def verMatriz (matriz,url,etiqueta):
+     # borramos todo lo dibujado anteriormente
+     canvas.delete(etiqueta)
+     for i, fila in enumerate(matriz):
+        for j, valor in enumerate(fila):
+            if valor == '2':
+                dibujarImagen(url[0],etiqueta,45,45,j*cell_width, i*cell_height)
+            if valor == '1':
+                dibujarImagen(url[1],etiqueta,45,45,j*cell_width, i*cell_height)
+            if valor == '6':
+                dibujarImagen(url[2],etiqueta,45,45,j*cell_width, i*cell_height)
+
+# 2.  Mostrar el estado inicial  
+#Función que mostrará la matriz incial
+def verMatrizInical():
+    verMatriz(matriz, ["../imagenes/gokú.png","../imagenes/muro.png","../imagenes/esfera.png"],'imagen')
+
+verMatrizInical()
+
+#Función que permitirá visualizar la animación
+def animacion ():
+    for matriz in caminos_final[1:]:
+        verMatriz(matriz,["../imagenes/gokú.png","../imagenes/muro.png","../imagenes/esfera.png"],'imagen')
+        time.sleep(0.1)
+        ventana.update()
+    mostrarReporte()    
+
+#Agregar botones a la ventana y ejecutar función a darles click
+btn = tk.Button(ventana, text='Empezar', command = animacion, bg='black', fg='white', font=("Arial", 12))         
+btn.place(x=135,y=590, width=150, height=50)
+btn2 = tk.Button(ventana, text='Volver', command = verMatrizInical, bg='black', fg='white', font=("Arial", 12))         
+btn2.place(x=310,y=590, width=150, height=50)
+
+ventana.mainloop()
+
+
+
 
 
 
